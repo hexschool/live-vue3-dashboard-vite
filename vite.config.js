10d42c9908c2fb3311500c19c7155990bdf26fc6
yinmin <alice0050722@gmail.com>
2023-02-16 10:12:27 +0800

    fix: 增加 env 與 config base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/live-vue3-dashboard-vite/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
